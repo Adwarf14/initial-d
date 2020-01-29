be02fa34891fa79abb82f05fb9aa4ad228611aa0
Adrien Imberdis <AdrienImberdis@gmail.com>
2020-01-29 14:23:07 +0100

    init of backend w/ first express route as exemple

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 5050;
+const connection = require("./conf");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.get("/api/persos", (req, res) => {
+  connection.query("SELECT * from perso", (err, results) => {});
+});
+
+app.listen(port, err => {
+  if (err) {
+    throw new Error("Something bad happened...");
+  }
+
+  console.log(`Server is listening on ${port}`);
+});
