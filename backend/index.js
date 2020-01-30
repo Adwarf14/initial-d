cf7af25c4f4d708d2748ef9ee699e5cc1ee7f4f0
Adrien Imberdis <AdrienImberdis@gmail.com>
2020-01-30 17:44:48 +0100

    confi

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 3306;
-const connection = require("./conf");
+const { db, backendPort } = require("./conf");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.use(bodyParser.json());
 app.use(
@@ -10,12 +10,29 @@ app.use(
     extended: true
   })
 );
+app.use(cors());
 
 app.get("/api/persos", (req, res) => {
-  connection.query("SELECT * from perso", (err, results) => {});
+  db.query("SELECT * from perso", (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur lors de la récupération des personnages");
+    } else {
+      res.status(200).json(results);
+    }
+  });
 });
 
-app.listen(port, err => {
+app.get("/api/teams", (req, res) => {
+  db.query("SELECT * from team", (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur lors de la récupération des personnages");
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+app.listen(backendPort, err => {
   if (err) {
     throw new Error("Something bad happened...");
   }
